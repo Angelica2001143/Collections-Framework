d69d7ff5a202f20448e8ee9dc36d659561713807
Angelica Quilme Manlapaz <77945056+Angelica2001143@users.noreply.github.com>
2021-04-21 09:48:19 +0800

    Number8Main

---
+++ Java0420-2021/src/java0420/pkg2021/Number8Main.java
@@ -0,0 +1,14 @@
+
+package java0420.pkg2021;
+import java.util.*;
+import static java0420.pkg2021.Number8.CountNumberKeys;
+
+
+public class Number8Main {
+
+    
+    public static void main(String[] args) {
+         CountNumberKeys();
+    }
+    
+}
